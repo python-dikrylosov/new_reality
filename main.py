e528ff6c31b3cb9ca308452b08c8afff87f3c5c4
python_dikrylosov <48491372+python-dikrylosov@users.noreply.github.com>
2023-05-23 03:23:19 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import time
 import Bwork_voice
 now_start = "Запустить сейчас"
 x = 0 
 for i in range(1):
     Bwork_voice.speek_text("Ожидаем сигнала")
+    time.sleep(3)
     if x >= 1:
         Bwork_voice.speek_text(now_start)
     x = x + 1
